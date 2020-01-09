9c8aba7dc813288e2c89c708b3983880caa7f818
PaKas <KasymbekovPN@yandex.ru>
2020-01-09 23:40:53 +0300

    * change loading some config-files (clientConfig.json, messageGroups.json). + add content checking of this files.

--- zuiNotes-messageSystem/src/main/java/ru/otus/kasymbekovPN/zuiNotesMS/config/SocketHandlerConfig.java
+++ zuiNotes-messageSystem/src/main/java/ru/otus/kasymbekovPN/zuiNotesMS/config/SocketHandlerConfig.java
@@ -24,6 +24,8 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @RequiredArgsConstructor
@@ -31,6 +33,14 @@ public class SocketHandlerConfig {
 
     private static final String MESSAGE_GROUPS = "messageGroups.json";
     private static final String MS_PORT_ARG_NAME = "ms.port";
+    private static final String COMMON_FIELD = "common";
+    private static final String REGISTRATION_FIELD = "registration";
+    private static final String ECHO_FIELD = "echo";
+    private static final Set<String> CONFIG_ARRAY_NAMES = new HashSet<String>(){{
+        add(COMMON_FIELD);
+        add(REGISTRATION_FIELD);
+        add(ECHO_FIELD);
+    }};
 
     private final MessageSystem messageSystem;
     private final MsClientService msClientService;
@@ -45,45 +55,10 @@ public class SocketHandlerConfig {
             throw new Exception(clArgsParser.getStatus());
         }
 
-        //<
-        String status = "";
-        String content = "";
-        URL resource = getClass().getClassLoader().getResource(MESSAGE_GROUPS);
-        if (resource != null){
-            File file = new File(resource.getFile());
-            try{
-                content = new String(Files.readAllBytes(file.toPath()));
-            } catch (IOException ex){
-                status = "JsonCheckerImpl : Failed convert file ("+ MESSAGE_GROUPS +") to string";
-                throw new IOException(status);
-            }
-        } else {
-            status = "JsonCheckerImpl : File "+ MESSAGE_GROUPS + " doesn't exist";
-            throw new Exception(status);
-        }
-
-        //<
-//        {
-//            "common" : [
-//            "AUTH_USER",
-//                    "ADD_USER",
-//                    "DEL_USER"
-//    ],
-//            "registration" : [
-//            "I_AM"
-//    ],
-//            "echo" : [
-//            "ECHO"
-//    ]
-
-        JsonObject loadedJsonObject = (JsonObject) new JsonParser().parse(content);
-//        JsonArray registrationMessages = loadedJsonObject.get("registrationMessages").getAsJsonArray();
-//        JsonArray commonMessages = loadedJsonObject.get("commonMessages").getAsJsonArray();
-        //<
-        JsonArray commonMessages = loadedJsonObject.get("common").getAsJsonArray();
-        JsonArray registrationMessages = loadedJsonObject.get("registration").getAsJsonArray();
-        JsonArray echoMessages = loadedJsonObject.get("echo").getAsJsonArray();
-        //<
+        JsonObject config = loadAndCheckConfig();
+        JsonArray commonMessages = config.get(COMMON_FIELD).getAsJsonArray();
+        JsonArray registrationMessages = config.get(REGISTRATION_FIELD).getAsJsonArray();
+        JsonArray echoMessages = config.get(ECHO_FIELD).getAsJsonArray();
 
         SocketHandlerImpl socketHandler = new SocketHandlerImpl(new JsonCheckerImpl(), new MSSocketSendingHandler(msPort), msPort);
 
@@ -113,5 +88,50 @@ public class SocketHandlerConfig {
         msClientService.setSocketHandler(socketHandler);
 
         return socketHandler;
+    }
+
+    private JsonObject loadAndCheckConfig() throws Exception {
+        JsonObject jsonObject = new JsonObject();
+        URL resource = getClass().getClassLoader().getResource(MESSAGE_GROUPS);
+        if (resource != null){
+            File file = new File(resource.getFile());
+            try{
+                jsonObject = (JsonObject) new JsonParser().parse(
+                        new String(Files.readAllBytes(file.toPath()))
+                );
+            } catch (IOException ex){
+                throw new IOException("JsonCheckerImpl : Failed convert file ("+ MESSAGE_GROUPS +") to string");
+            }
+        } else {
+            throw new Exception("JsonCheckerImpl : File "+ MESSAGE_GROUPS + " doesn't exist");
+        }
+
+        StringBuilder status = new StringBuilder();
+        for (String configArrayName : CONFIG_ARRAY_NAMES) {
+            if (jsonObject.has(configArrayName)){
+                JsonElement element = jsonObject.get(configArrayName);
+                if (element.isJsonArray()){
+                    JsonArray array = element.getAsJsonArray();
+                    for (JsonElement arrayElement : array) {
+                        if (!arrayElement.isJsonPrimitive() ||
+                            !arrayElement.getAsJsonPrimitive().isString()){
+
+                            status.append(configArrayName).append(" contains element '").append(arrayElement.toString())
+                                    .append("' which isn't string | ");
+                        }
+                    }
+                } else {
+                    status.append(configArrayName).append(" isn't array | ");
+                }
+            } else {
+                status.append(MESSAGE_GROUPS).append(" doesn't contain string array ").append(configArrayName).append(" | ");
+            }
+        }
+
+        if (!status.toString().isEmpty()){
+            throw new Exception(status.toString());
+        }
+
+        return jsonObject;
     }
 }
--- zuiNotes-messageSystem/src/main/java/ru/otus/kasymbekovPN/zuiNotesMS/messageSystem/client/creation/factory/MsClientCreatorFactoryImpl.java
+++ zuiNotes-messageSystem/src/main/java/ru/otus/kasymbekovPN/zuiNotesMS/messageSystem/client/creation/factory/MsClientCreatorFactoryImpl.java
@@ -2,53 +2,30 @@ package ru.otus.kasymbekovPN.zuiNotesMS.messageSystem.client.creation.factory;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.springframework.stereotype.Service;
 import ru.otus.kasymbekovPN.zuiNotesMS.messageSystem.client.creation.creator.CmnMsClientCreator;
 import ru.otus.kasymbekovPN.zuiNotesMS.messageSystem.client.creation.creator.MsClientCreator;
 import ru.otus.kasymbekovPN.zuiNotesMS.messageSystem.client.creation.creator.WrongMsClientCreator;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.Set;
 
-@Service
 public class MsClientCreatorFactoryImpl implements MsClientCreatorFactory {
 
-    private static final String CLIENT_CONFIG = "clientConfig.json";
     private final JsonObject config;
+    private final String solusField;
+    private final String messagesField;
 
-    public MsClientCreatorFactoryImpl() throws Exception {
-
-        String status = "";
-        String content = "";
-        URL resource = getClass().getClassLoader().getResource(CLIENT_CONFIG);
-        if (resource != null){
-            File file = new File(resource.getFile());
-            try{
-                content = new String(Files.readAllBytes(file.toPath()));
-            } catch (IOException ex){
-                status = "JsonCheckerImpl : Failed convert file ("+ CLIENT_CONFIG +") to string";
-                throw new IOException(status);
-            }
-        } else {
-            status = "JsonCheckerImpl : File "+ CLIENT_CONFIG + " doesn't exist";
-            throw new Exception(status);
-        }
-
-        //< add checking
-        this.config = (JsonObject) new JsonParser().parse(content);
-        //<
+    public MsClientCreatorFactoryImpl(JsonObject config, String solusField, String messagesField) {
+        this.config = config;
+        this.solusField = solusField;
+        this.messagesField = messagesField;
     }
 
     @Override
     public MsClientCreator get(String entity) {
         if (config.has(entity)){
             Set<String> messages = new HashSet<>();
-            for (JsonElement element : config.get(entity).getAsJsonObject().get("messages").getAsJsonArray()) {
+            for (JsonElement element : config.get(entity).getAsJsonObject().get(messagesField).getAsJsonArray()) {
                 messages.add(element.getAsString());
             }
             return new CmnMsClientCreator(messages);
